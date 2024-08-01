97d0cb74cdbda4c3ef512727f775c0e508e06b73
nada-r <nadarzer@gmail.com>
2024-08-01 23:37:07 +0200

    load nft image on arweave

--- solana-starter/ts/cluster1/nft_image.ts
+++ solana-starter/ts/cluster1/nft_image.ts
@@ -3,11 +3,15 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
 import { readFile } from "fs/promises"
+import bs58 from "bs58"
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
 
-let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+// Decode the base58 secret key
+const secretKey = bs58.decode(wallet.skey);
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(secretKey));
 const signer = createSignerFromKeypair(umi, keypair);
 
 umi.use(irysUploader());
@@ -19,10 +23,14 @@ umi.use(signerIdentity(signer));
         //2. Convert image to generic file.
         //3. Upload image
 
-        // const image = ???
+        const image = await readFile('./assets/generug.png');
+        const generic = createGenericFile(image, 'rug', {
+            contentType: "image/png"
+        });
 
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
+        const [myUri] = await umi.uploader.upload([generic])
+
+        console.log("Your image URI: ", myUri);
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
