9bb0ee41e58f8f79a0eb6d983f703163a32b09ed
nada-r <nadarzer@gmail.com>
2024-08-03 23:52:50 +0200

    add mint functions

--- solana-starter/ts/cluster1/nft_mint.ts
+++ solana-starter/ts/cluster1/nft_mint.ts
@@ -3,12 +3,15 @@ import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount 
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
 import wallet from "../wba-wallet.json"
-import base58 from "bs58";
+import bs58 from "bs58";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
 const umi = createUmi(RPC_ENDPOINT);
 
-let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+// Decode the base58 secret key
+const secretKey = bs58.decode(wallet.skey);
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(secretKey));
 const myKeypairSigner = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(myKeypairSigner));
 umi.use(mplTokenMetadata())
@@ -16,11 +19,24 @@ umi.use(mplTokenMetadata())
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
+    let tx = createNft(umi,{
+        mint,
+        name: "nadarrugs gud",
+        symbol: "rugud",
+        uri: "https://arweave.net/cYt3MPr1fUX3YARmDzImfDJWV5CpHUw9Zjg7glMXf0c",
+        sellerFeeBasisPoints: percentAmount(5, 2),
+        creators: [
+            {
+                address: keypair.publicKey,
+                verified: true,
+                share: 100
+            }
+        ]
+    })
+    let result = await tx.sendAndConfirm(umi);
+    const signature = bs58.encode(result.signature);
     
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
 
     console.log("Mint Address: ", mint.publicKey);
 })();
